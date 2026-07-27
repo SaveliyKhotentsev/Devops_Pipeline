59c1eca738bd1614583b2706f6fec3599f94b9e0
SaveliyKhotentsev <saba.khot@mail.ru>
2026-07-27 17:10:37 +0300

    fix pytetst

--- tests/test_app.py
+++ tests/test_app.py
@@ -1,5 +1,9 @@
 import pytest
-from app import app as flask_app, tasks
+import os
+import sys
+
+sys.path.insert(0, os.path.abspath(os.path.join(os.path.dirname(__file__), "..")))
+from app import app as flask_app
 
 #Fixture — это объект, который pytest создает перед тестом.
 #То есть для фйнкций: app эквивалентно flask_app
@@ -28,4 +32,4 @@ def test_version(app, client):
 def test_health(app, client):
     response = client.get('/health')
     assert response.status_code == 200
-    assert response.json['status'] == 'UP'
+    assert response.json['status'] == 'OK'
